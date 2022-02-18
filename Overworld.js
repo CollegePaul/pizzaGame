502ffca63c566e8c060afb61746dced861f5aea4
CollegePaul <psmith@rnngroup.ac.uk>
2022-02-18 09:41:23 +0000

    cut scenes done

--- Overworld.js
+++ Overworld.js
@@ -6,54 +6,58 @@ class Overworld {
    this.map = null;
  }
 
- startGameLoop(){
-  const step = () => {
-    
+  startGameLoop() {
+    const step = () => {
+      //Clear off the canvas
+      this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
-    //clear the canvas
-    this.ctx.clearRect(0,0, this.canvas.width,this.canvas.height)
-    
-    //establish camera object - can be any npc also
-    const cameraPerson = this.map.gameOjects.hero;
+      //Establish the camera person
+      const cameraPerson = this.map.gameObjects.hero;
 
-    //update all objects
-    Object.values(this.map.gameOjects).forEach(object => {
-      object.update({
-        arrow: this.directionInput.direction,
-        map: this.map,
+      //Update all objects
+      Object.values(this.map.gameObjects).forEach(object => {
+        object.update({
+          arrow: this.directionInput.direction,
+          map: this.map,
+        })
       })
-    })
 
+      //Draw Lower layer
+      this.map.drawLowerImage(this.ctx, cameraPerson);
 
-    //maps
-    this.map.drawLowerImage(this.ctx, cameraPerson)
-    
-
-    //draw all objects
-    Object.values(this.map.gameOjects).forEach(object => {
+      //Draw Game Objects
+      Object.values(this.map.gameObjects).sort((a,b) => {
+        return a.y - b.y;
+      }).forEach(object => {
         object.sprite.draw(this.ctx, cameraPerson);
-    })
+      })
 
-    //draw map upper Layer
-    this.map.drawUpperImage(this.ctx, cameraPerson)
-
+      //Draw Upper layer
+      this.map.drawUpperImage(this.ctx, cameraPerson);
+      
       requestAnimationFrame(() => {
-      step();
-    })
-  }
-  step()
+        step();   
+      })
+    }
+    step();
  }
 
  init() {
   this.map = new OverworldMap(window.OverworldMaps.DemoRoom);
   this.map.mountObjects();
-  
-  this.directionInput = new directionInput();
+
+  this.directionInput = new DirectionInput();
   this.directionInput.init();
+
   this.startGameLoop();
 
-  
+  this.map.startCutscene([
+    { who: "hero", type: "walk",  direction: "down" },
+    { who: "hero", type: "walk",  direction: "down" },
+    { who: "npcA", type: "walk",  direction: "left" },
+    { who: "npcA", type: "walk",  direction: "left" },
+    { who: "npcA", type: "stand",  direction: "up", time: 800 },
+  ])
+
  }
-
 }
-
